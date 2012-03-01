327657ff59ca1bf76f2c45dc5df894c240acd2dd
Radek Hušek <PitelVonSacek@gmail.com>
2012-03-01 20:01:16 +0100

    Fix atomics

--- atomic.h
+++ atomic.h
@@ -1,13 +1,56 @@
 #ifndef __ATOMIC_H__
 #define __ATOMIC_H__
 
+#if 1
+
+#include "atomic_amd64.h"
+
+#define atomic_read_relaxed(ptr) atomic_read(ptr)
+#define atomic_write_relaxed(...) atromic_write(__VA_ARGS__)
+
+#define atomic_read(ptr) (*((volatile typeof(ptr))(ptr)))
+#define atomic_write(ptr, value) (atomic_read(ptr) = (value))
+#define atomic_barrier() asm volatile ()
+
+#define atomic_cmpswp(ptr, old, new_) \
+  _atomic_cmpxchg(ptr, old, new_, sizeof(*(ptr)), "lock; ")
+
+#define atomic_swp(ptr, new_) \
+  _atomic_xchg(new_, ptr, sizeof(*(ptr)))
+
+#define atomic_add_and_fetch(ptr, x) \
+  ({ \
+    typeof(*(ptr)) __x = (x); \
+    __x + _atomic_xadd(ptr, __x, "lock; "); \
+  })
+
+#define atomic_fetch_and_add(ptr, x) \
+  _atomic_xadd(ptr, x, "lock; ")
+
+#define atomic_add(ptr, x) ({ _atomic_xadd(ptr, x, "lock; "); (void)0; })
+
+#define atomic_inc(ptr) atomic_add(ptr, 1)
+#define atomic_dec(ptr) atomic_add(ptr, -1)
+
+#else // USE_ATOMIC_BUILTINS
+
 #define atomic_read(ptr) __sync_fetch_and_add(ptr, 0)
+#define atomic_write(ptr, val) ((void)atomic_swp(ptr, val));
+#define atomic_barrier() __sync_synchronize()
 
 #define atomic_cmpswp(ptr, old, new_) \
   __sync_bool_compare_and_swap(ptr, old, new_)
 
+// ugly & ineffective one
+#define atomic_swp(ptr, val) __sync_val_compare_and_swap(ptr, *ptr, val)
+
+#define atomic_add_and_fetch(ptr, x) __sync_add_and_fetch(ptr, x)
+
+#define atomic_add(ptr, x) atomic_add_and_fetch(ptr, x)
+
 #define atomic_inc(ptr) __sync_add_and_fetch(ptr, 1)
 #define atomic_dec(ptr) __sync_add_and_fetch(ptr, -1)
+#endif
 
 #endif
 
---
+++ atomic_amd64.h
@@ -0,0 +1,163 @@
+/*
+ * Implements atomic operations for amd64 platform.
+ *
+ * Based on implementation of atomics in Linux kernel.
+ *
+ * Relevant files: arch/x86/asm/{atomic,cmpxchg}.h
+ *
+ * ?? In original files have no explicit license, so they're under GPL ??
+ */
+
+#ifndef __ATOMIC_AMD64_H__
+#define __ATOMIC_AMD64_H__
+
+/*
+ * Non-existant functions to indicate usage errors at link time
+ * (or compile-time if the compiler implements __compiletime_error().
+ */
+extern void __xchg_wrong_size(void);
+extern void __cmpxchg_wrong_size(void);
+extern void __xadd_wrong_size(void);
+
+#define __X86_CASE_B	1
+#define __X86_CASE_W	2
+#define __X86_CASE_L	4
+#define __X86_CASE_Q	8
+
+/*
+ * Note: no "lock" prefix even on SMP: xchg always implies lock anyway.
+ * Since this is generally used to protect other memory information, we
+ * use "asm volatile" and "memory" clobbers to prevent gcc from moving
+ * information around.
+ */
+#define _atomic_xchg(x, ptr, size)						\
+({									\
+	__typeof(*(ptr)) __x = (x);					\
+	switch (size) {							\
+	case __X86_CASE_B:						\
+	{								\
+		volatile uint8_t *__ptr = (volatile uint8_t *)(ptr);		\
+		asm volatile("xchgb %0,%1"				\
+			     : "=q" (__x), "+m" (*__ptr)		\
+			     : "0" (__x)				\
+			     : "memory");				\
+		break;							\
+	}								\
+	case __X86_CASE_W:						\
+	{								\
+		volatile uint16_t *__ptr = (volatile uint16_t *)(ptr);		\
+		asm volatile("xchgw %0,%1"				\
+			     : "=r" (__x), "+m" (*__ptr)		\
+			     : "0" (__x)				\
+			     : "memory");				\
+		break;							\
+	}								\
+	case __X86_CASE_L:						\
+	{								\
+		volatile uint32_t *__ptr = (volatile uint32_t *)(ptr);		\
+		asm volatile("xchgl %0,%1"				\
+			     : "=r" (__x), "+m" (*__ptr)		\
+			     : "0" (__x)				\
+			     : "memory");				\
+		break;							\
+	}								\
+	case __X86_CASE_Q:						\
+	{								\
+		volatile uint64_t *__ptr = (volatile uint64_t *)(ptr);		\
+		asm volatile("xchgq %0,%1"				\
+			     : "=r" (__x), "+m" (*__ptr)		\
+			     : "0" (__x)				\
+			     : "memory");				\
+		break;							\
+	}								\
+	default:							\
+		__xchg_wrong_size();					\
+	}								\
+	__x;								\
+})
+
+/*
+ * Atomic compare and exchange.  Compare OLD with MEM, if identical,
+ * store NEW in MEM.  Return the initial value in MEM.  Success is
+ * indicated by comparing RETURN with OLD.
+ */
+#define _atomic_cmpxchg(ptr, old, new, size, lock)			\
+({									\
+	__typeof__(*(ptr)) __ret;					\
+	__typeof__(*(ptr)) __old = (old);				\
+	__typeof__(*(ptr)) __new = (new);				\
+	switch (size) {							\
+	case __X86_CASE_B:						\
+	{								\
+		volatile uint8_t *__ptr = (volatile uint8_t *)(ptr);		\
+		asm volatile(lock "cmpxchgb %2,%1"			\
+			     : "=a" (__ret), "+m" (*__ptr)		\
+			     : "q" (__new), "0" (__old)			\
+			     : "memory");				\
+		break;							\
+	}								\
+	case __X86_CASE_W:						\
+	{								\
+		volatile uint16_t *__ptr = (volatile uint16_t *)(ptr);		\
+		asm volatile(lock "cmpxchgw %2,%1"			\
+			     : "=a" (__ret), "+m" (*__ptr)		\
+			     : "r" (__new), "0" (__old)			\
+			     : "memory");				\
+		break;							\
+	}								\
+	case __X86_CASE_L:						\
+	{								\
+		volatile uint32_t *__ptr = (volatile uint32_t *)(ptr);		\
+		asm volatile(lock "cmpxchgl %2,%1"			\
+			     : "=a" (__ret), "+m" (*__ptr)		\
+			     : "r" (__new), "0" (__old)			\
+			     : "memory");				\
+		break;							\
+	}								\
+	case __X86_CASE_Q:						\
+	{								\
+		volatile uint64_t *__ptr = (volatile uint64_t *)(ptr);		\
+		asm volatile(lock "cmpxchgq %2,%1"			\
+			     : "=a" (__ret), "+m" (*__ptr)		\
+			     : "r" (__new), "0" (__old)			\
+			     : "memory");				\
+		break;							\
+	}								\
+	default:							\
+		__cmpxchg_wrong_size();					\
+	}								\
+	__ret;								\
+})
+
+#define _atomic_xadd(ptr, inc, lock)						\
+	({								\
+	  __typeof__ (*(ptr)) __ret = (inc);			\
+		switch (sizeof(*(ptr))) {				\
+		case __X86_CASE_B:					\
+			asm volatile (lock "xaddb %b0, %1\n"		\
+				      : "+r" (__ret), "+m" (*(ptr))	\
+				      : : "memory", "cc");		\
+			break;						\
+		case __X86_CASE_W:					\
+			asm volatile (lock "xaddw %w0, %1\n"		\
+				      : "+r" (__ret), "+m" (*(ptr))	\
+				      : : "memory", "cc");		\
+			break;						\
+		case __X86_CASE_L:					\
+			asm volatile (lock "xaddl %0, %1\n"		\
+				      : "+r" (__ret), "+m" (*(ptr))	\
+				      : : "memory", "cc");		\
+			break;						\
+		case __X86_CASE_Q:					\
+			asm volatile (lock "xaddq %q0, %1\n"		\
+				      : "+r" (__ret), "+m" (*(ptr))	\
+				      : : "memory", "cc");		\
+			break;						\
+		default:						\
+			__xadd_wrong_size();				\
+		}							\
+		__ret;							\
+	})
+
+#endif
+
