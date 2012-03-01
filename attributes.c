beab74a1910f42e5d63f2e346b810302e08ff5fd
Radek Hušek <PitelVonSacek@gmail.com>
2012-03-01 22:47:14 +0100

    Delete crap

--- attributes.c
+++
@@ -1,7 +0,0 @@
-#include "database.h"
-
-#undef ATTRIBUTES_H
-
-#define IMPLEMENT_ATTRIBUTES
-#include "attributes.h"
-
--- exception.c
+++
@@ -1,8 +0,0 @@
-#include "exception.h"
-
-__thread ExceptionHandler *exception_handler = 0;
-__thread const Exception *exception_exception = 0;
-__thread void *exception_info;
-
-EXCEPTION_DEFINE(OOPS, NONE);
-
