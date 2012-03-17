PitelVonSacek/HybridDatabases

commit f18a02b410b691d10583c589062349db59733959

Fix potential stric-aliasing issues in fast_stack & num_dictionary

utils/num_dictionary.h
@@ -47,7 +47,7 @@ typedef struct {
 #define _ndict_uncast(dict) \
   static_if( \
     types_equal(typeof((dict)->__dummy), IsNumDictionaryDummy), \
-    (NumDictionary*)(dict), \
+    utilCast(struct GenericDictionary, dict), \
     (void)0 \
   )
 
@@ -86,7 +86,8 @@ typedef struct {
     __node->generic_key = 0; \
     __node->key = (key_); \
     __node->value = (value_); \
-    ndict_generic_insert(_ndict_uncast(dict), (struct NumDictionaryNode*)__node); \
+    ndict_generic_insert(_ndict_uncast(dict), \
+                         utilCast(struct NumDictionaryNode, __node)); \
   })
 
 #define ndict_remove(dict, key) \

utils/fast_stack.h
@@ -74,7 +74,7 @@ typedef struct {} IsFastStack;
     typeof(&*(stack)) __stack = (stack); \
     STATIC_ASSERT(types_equal(typeof(__stack->is_fast_stack), IsFastStack)); \
     if (__stack->ptr == __stack->end) \
-      _fstack_expand((GenericFastStack*)__stack, _fstack_offsets); \
+      _fstack_expand(utilCast(GenericFastStack, __stack), _fstack_offsets); \
     *__stack->ptr++ = (item); \
     (void)0; \
   })
@@ -90,7 +90,7 @@ typedef struct {} IsFastStack;
     typeof(&*(stack)) __stack = (stack); \
     STATIC_ASSERT(types_equal(typeof(__stack->is_fast_stack), IsFastStack)); \
     if (--__stack->ptr == __stack->begin) \
-      _fstack_shrink((GenericFastStack*)__stack, _fstack_offsets); \
+      _fstack_shrink(utilCast(GenericFastStack, __stack), _fstack_offsets); \
     (void)0; \
   })
 

commit 6a715368fdf102888821666337f256648d1d81ec

Fix bug in num_dictionary introduced in commit f18a02b4

utils/num_dictionary.h
@@ -47,7 +47,7 @@ typedef struct {
 #define _ndict_uncast(dict) \
   static_if( \
     types_equal(typeof((dict)->__dummy), IsNumDictionaryDummy), \
-    utilCast(struct GenericDictionary, dict), \
+    utilCast(NumDictionary, dict), \
     (void)0 \
   )
 