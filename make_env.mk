9c3b356982b4f2d9b0cc919e3cf07b2dac2eb223
Radek Hušek <PitelVonSacek@gmail.com>
2012-03-17 16:31:53 +0100

    Add -O2 to default gcc options

--- make_env.mk
+++ make_env.mk
@@ -2,5 +2,5 @@ CC = gcc -std=gnu99
 WARNINGS += -Wall -Wuninitialized -Winit-self -Wcast-align \
             -Wno-format-zero-length -Wno-parentheses
 
-CFLAGS += ${WARNINGS} -ggdb
+CFLAGS += ${WARNINGS} -ggdb -O2
 
