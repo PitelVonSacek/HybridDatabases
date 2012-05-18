e46fc63c0695bd8bb6b0e5965de49a7cba677440
Radek Hušek <PitelVonSacek@gmail.com>
2012-05-18 10:52:31 +0200

    Document utils/bit_array.h

--- utils/bit_array.h
+++ utils/bit_array.h
@@ -1,10 +1,23 @@
 #ifndef __BIT_ARRAY_H__
 #define __BIT_ARRAY_H__
+/**
+ * @file
+ * @breif Definice datové struktury bitové pole.
+ *
+ * Implementace inspirována bitovým polem v knihovně libucw
+ * (http://www.ucw.cz/libucw/).
+ */
 
 #include "basic_utils.h"
 
+/// Prázdná struktura, slouží pouze pro typovou kontrolu.
 typedef struct {} IsBitArray;
 
+/**
+ * Definuje typ bitového pole dané velikosti.
+ * Velikost pole je uložena v elementu .size (tento element
+ * má nulovou velikost, viz #StaticInt).
+ */
 #define BitArray(size_) \
   struct { \
     uint32_t data[(size_ + 31) / 32]; \
@@ -12,6 +25,7 @@ typedef struct {} IsBitArray;
     IsBitArray is_bit_array; \
   }
 
+/// Inicializuje bitové pole (nastaví všechny bity na 0).
 #define bit_array_init(array) \
   do { \
     typeof(&*(array)) __array = (array); \
@@ -19,25 +33,34 @@ typedef struct {} IsBitArray;
     memset(__array->data, 0, sizeof(__array->data)); \
   } while (0)
 
+/// Destruktor, nedělá nic.
 #define bit_array_destroy(array) do {} while (0)
 
+/// Vynuluje celé pole
 #define bit_array_erase(array) bit_array_init(array)
 
+
+/// Zjistí velikost bitového pole
 #define bit_array_size(array) StaticGetInt((array)->size)
+/// Počet slov typu #uint32_t, v nichž je pole uloženo.
 #define _bit_array_words(a) (sizeof((a)->data) / sizeof((a)->data[0]))
 
+
+/// Nastaví bit @c i na 1.
 #define bit_array_set(array, i) \
   do { \
     STATIC_ASSERT(types_equal(typeof((array)->is_bit_array), IsBitArray)); \
     ((array)->data)[ i / 32 ] |= (1 << (i % 32)); \
   } while (0)
 
+/// Vrátí hodnotu bitu @c i.
 #define bit_array_get(array, i) \
   ({ \
     STATIC_ASSERT(types_equal(typeof((array)->is_bit_array), IsBitArray)); \
     ((array)->data)[ i / 32 ] & (1 << (i % 32)); \
   })
 
+/// Zneguje hodnotu bitu @c i.
 #define bit_array_flip(array, i) \
   do { \
     STATIC_ASSERT(types_equal(typeof((array)->is_bit_array), IsBitArray)); \
@@ -45,6 +68,7 @@ typedef struct {} IsBitArray;
   } while (0)
 
 
+/// Spolu s #bitArrayForEnd iteruje přes všechny bity pole s hodnotou 1.
 #define bitArrayFor(index, array) \
   do { \
     __label__ _bit_array_break, _bit_array_continue; \
