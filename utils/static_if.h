bc0be34d61e11c6c9c251893bdc54cfd79887589
Radek Hušek <PitelVonSacek@gmail.com>
2012-05-18 13:36:28 +0200

    Document & clean up utils/static_if.h & utils/type_magic.h

--- utils/static_if.h
+++ utils/static_if.h
@@ -1,38 +1,47 @@
 #ifndef __STATIC_IF_H__
 #define __STATIC_IF_H__
 
-typedef struct {} StaticTrue;
-typedef struct {} StaticFalse;
+/**
+ * @file
+ * @brief Drobnosti pro vyhodnocování výrazů při překladu.
+ *
+ * Inspirováno zvrhlostmi z knihovny Boost.
+ */
 
-#define StaticIsTrue(var) types_equal(typeof(var), StaticTrue)
 
-
-
+/**
+ * Vrátí datový typ, v němž je ulžené číslo @c val.
+ * Proměnná totho typu má nulovou velikost.
+ */
 #define StaticInt(val) \
   struct { struct { char value[val]; } _static_int[0]; }
+/// Z proměnné typu StaticInt() vytáhne hodnotu.
 #define StaticGetInt(var) sizeof((var)._static_int->value)
 
 
+/**
+ * Typový if.
+ *
+ * V závyslosti na pravdivostní hodnotě podmíky, vrací typ A nebo B.
+ */
 #define StaticIf(cond, A, B) \
   typeof(static_if(cond, *((typeof(A)*)0), *((typeof(B)*)0)))
 
 
+/// Assert vyhodnocený při překladu.
 #define STATIC_ASSERT(x) switch (0) { case 0: case (x): ; }
 
+
+/**
+ * Konstrukce if vyhodnocená při překladu.
+ *
+ * Na rozdíl od ?: může mít každá z větví jiný typ.
+ */
 #define static_if(cond, a, b) __builtin_choose_expr((cond), (a), (b)) 
 
+
+/// Vrací @c true, pokud jsou typy shodné.
 #define types_equal(t1, t2) __builtin_types_compatible_p(t1, t2)
-
-#define type_switch(expr, opt) ({ \
-    typeof(expr) _type_switch_dummy; \
-    \
-    opt; \
-  })
-
-#define type_case(type, action, else_) static_if( \
-  types_equal(typeof(_type_switch_dummy), type), \
-  action, else_)
-
 
 #endif
 
--- utils/type_magic.h
+++ utils/type_magic.h
@@ -1,10 +1,22 @@
 #ifndef __TYPE_MAGIC_H__
 #define __TYPE_MAGIC_H__
 
+/**
+ * @file
+ * @brief Implementace podpory pro jednoúrovňovou dědičnost.
+ *
+ * Každý objekt obsahuje atribut @c __ancestor. Pokud je objekt
+ * kořenem hierarchie, je tento atribut typu DummyAncestor.
+ * Je-li podtřídou má typ nadřazené třídy a je to první atribut
+ * v objektu.
+ */
+
 #include "static_if.h"
 
+/// Typ označující, že třída je kořenem objektové hierarchie.
 typedef struct {} DummyAncestor;
 
+/// Přetypuje objekt na odpovídající abstraktní třídu.
 #define typeUncast(object) \
   static_if( \
     types_equal(typeof((object)->__ancestor), DummyAncestor), \
